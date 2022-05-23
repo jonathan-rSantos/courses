60cfacb2f6645adc47df49d597a232d400ddf503
Jonathan <jonathan.ribeiro28121997@gmail.com>
2022-05-22 21:31:30 -0300

    tratamento de erros e matDialog

--- src/app/shared/app-material/app-material.module.ts
+++ src/app/shared/app-material/app-material.module.ts
@@ -5,13 +5,17 @@ import { MatTableModule } from '@angular/material/table';
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   exports: [
     MatListModule,
     MatCardModule,
     MatToolbarModule,
     MatTableModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatDialogModule,
+    MatButtonModule
   ],
 })
 export class AppMaterialModule { }
